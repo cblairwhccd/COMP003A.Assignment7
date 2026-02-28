1e0c28a0b4cb46fdb8e20c1db3f345350e5a7900
cblairwhccd <christianblair@my.whccd.edu>
2026-02-27 21:44:42 -0800

    Add menu logic

--- Program.cs
+++ Program.cs
@@ -2,8 +2,52 @@
 
 class Program
 {
+    /* Prompt for integer within min/max range */
+    static int Prompt(string prompt, int min, int max)
+    {
+        int result;
+        while (true)
+        {
+            Console.Write(prompt);
+            try
+            {
+                result = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid selection.");
+                continue;
+            }
+            if (result < min || result > max)
+            {
+                Console.WriteLine("Selection out of range.");
+                continue;
+            }
+            break;
+        }
+        return result;
+    }
+
     static void Main()
     {
-        int[] scores = {85, 90, 78, 92,88};
+        int[] scores = {85, 90, 78, 92, 88};
+        string[] menu = {"Display Values", "Show Total", "Show Average", "Exit"};
+        for (int i = 0; i < menu.Length; i++)
+        {
+            Console.WriteLine($"{1+i}. {menu[i]}");
+        }
+        int choice;
+        while ((choice = Prompt("Enter choice: ", 1, 4)) != 4)
+        {
+            switch (choice)
+            {
+                case 1: /* Display Values */
+                    break;
+                case 2: /* Show Total */
+                    break;
+                case 3: /* Show Average */
+                    break;
+            }
+        }
     }
 }
